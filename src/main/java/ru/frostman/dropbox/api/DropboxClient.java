fcfc5e9c1d79e514dc479dbbe2dc229730d63d9e
slukjanov <me@frostman.ru>
2011-08-15 23:43:49 +0400

    getMetadata implemented

--- src/main/java/ru/frostman/dropbox/api/DropboxClient.java
+++ src/main/java/ru/frostman/dropbox/api/DropboxClient.java
@@ -5,15 +5,20 @@ import org.scribe.model.Token;
 import org.scribe.model.Verb;
 import org.scribe.oauth.OAuthService;
 import ru.frostman.dropbox.api.model.AccountInfo;
+import ru.frostman.dropbox.api.model.Entry;
 import ru.frostman.dropbox.api.util.Json;
 
+import javax.annotation.Nullable;
+
 import static ru.frostman.dropbox.api.util.DropboxError.check;
+import static ru.frostman.dropbox.api.util.DropboxError.checkMetadata;
 
 /**
  * @author slukjanov aka Frostman
  */
 public class DropboxClient {
     private static final String INFO_URL = "https://api.dropbox.com/0/account/info";
+    private static final String METADATA_URL = "https://api.dropbox.com/0/metadata/dropbox";
 
     private final OAuthService service;
     private final Token accessToken;
@@ -32,8 +37,43 @@ public class DropboxClient {
         service.signRequest(accessToken, request);
         String content = check(request.send()).getBody();
 
-        //todo think about errors handling
         return Json.parse(content, AccountInfo.class);
+    }
+
+    public Entry getMetadata(String path) {
+        return getMetadata(path, 10000, null, true);
+    }
+
+    public Entry getMetadata(String path, int fileLimit) {
+        return getMetadata(path, fileLimit, null, true);
+    }
+
+    public Entry getMetadata(String path, String hash) {
+        return getMetadata(path, 10000, hash, true);
+    }
+
+    public Entry getMetadata(String path, int fileLimit, @Nullable String hash) {
+        return getMetadata(path, fileLimit, hash, true);
+    }
+
+    public Entry getMetadata(String path, int fileLimit, @Nullable String hash, boolean list) {
+        OAuthRequest request = new OAuthRequest(Verb.GET, METADATA_URL + path);
+
+        if (fileLimit != 10000)
+            request.addQuerystringParameter("file_limit", Integer.toString(fileLimit));
+
+        if (hash != null) {
+            request.addQuerystringParameter("hash", hash);
+        }
+
+        if (!list) {
+            request.addQuerystringParameter("list", "false");
+        }
+
+        service.signRequest(accessToken, request);
+        String content = checkMetadata(request.send()).getBody();
+
+        return Json.parse(content, Entry.class);
     }
 
 }

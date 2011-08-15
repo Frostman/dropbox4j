3f0fddec82361ecdc73bbf77de2ba4c83b42fe0f
slukjanov <me@frostman.ru>
2011-08-15 23:12:58 +0400

    Maps helper implemented

---
+++ src/main/java/ru/frostman/dropbox/api/util/Maps.java
@@ -0,0 +1,14 @@
+package ru.frostman.dropbox.api.util;
+
+import java.util.HashMap;
+
+/**
+ * @author slukjanov aka Frostman
+ */
+public class Maps {
+
+    public static <K, V>HashMap<K, V> createHashMap(){
+        return new HashMap<K, V>();
+    }
+
+}
